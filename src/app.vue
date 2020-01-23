3728f3bf68ee82c8fd863f2c81ed887f0cb3b4cc
ikiiiiiiii <1053033472@qq.com>
2020-01-23 17:11:04 +0800

    购物车功能实现

--- src/app.vue
+++ src/app.vue
@@ -4,12 +4,17 @@
         <!-- 顶部区域 -->
         <div class="top">
 			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	        <div class="container-fluid">
-	            <div class="navbar-header top2">
-		            <a class="navbar-brand" href="#">这是一个导航栏</a>
-	            </div>
-	        </div>
-        </nav>
+				<div class="container-fluid">
+					<div class="navbar-header top2">
+						<a class="navbar-brand" href="#">这是一个导航栏</a>
+					</div>
+				</div>
+				<div>
+					<span slot="right" @click="goBack" v-show="flag">
+						<mt-button icon="back" class="navbar-brand">返回</mt-button>
+					</span>
+				</div>
+			</nav>
 		</div>
 
 
@@ -33,8 +38,8 @@
 			</li>
             <li>
 				<router-link to="/share">
-					<span class="glyphicon glyphicon-share"></span><br/>分享
-					<span class="badge" id="badge">0</span>
+					<span class="glyphicon glyphicon-share"></span><br/>购物车
+					<span class="badge" id="badge">{{ $store.getters.getAllCount }}</span>
 				</router-link>
 			</li>
             <li>
@@ -49,16 +54,41 @@
 </template>
 
 <script>
-
+export default{
+	data(){
+		return {
+			flag: false
+		};
+	},
+	created(){
+		this.flag = this.$route.path === '/home'? false : true;
+	},
+	methods: {
+		goBack(){
+			this.$router.go(-1)
+		}
+	},
+	watch: {
+		"$route.path": function(newVal){
+			if (newVal === "/home"){
+				this.flag = false;
+			}else{
+				this.flag = true;
+			}
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
 /* 修改导航栏高度 */
 .top .navbar {
     min-height:80px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 .top2 .navbar-brand {
-    padding-top:30px ; 
     padding-bottom:0 ;
 	font-size: 25px;
 }
